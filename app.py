7993c014f04b44d955e0059ccbfdc63db2e68e43
poornesh <ks.poornesh@gmail.com>
2025-07-31 14:57:09 +0530

    Fix: use dynamic port for Azure

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import logging
+import os
 
 app = Flask(__name__)
 
@@ -21,4 +22,5 @@ def log():
     return "Log generated", 200
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
